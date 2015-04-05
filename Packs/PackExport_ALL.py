32e425685afbbc4ac2a77708061c85ca3f862b4d
Superfranci99 <superfranci99@gmail.com>
2015-04-05 12:07:57 +0200

    Updated .gitignore

--- Packs/PackExport_ALL.py
+++ Packs/PackExport_ALL.py
@@ -4,13 +4,18 @@
 import xml.etree.ElementTree as ET
 import os
 
+out = "FileData"
+
 tree = ET.parse("DearlyStarsPack.xml")
 root = tree.getroot()
+
+if not os.path.exists(out):
+        os.makedirs(out)
 
 for i in root.iter("Pack"):
     eztFile = i.find("EZT").text
     ezpFile = i.find("EZP").text
-    outputDir = i.find("Output").text
+    outputDir = os.path.join(out, i.find("Output").text)
     # create folder for the subfiles, named as outputDir
     if not os.path.exists(outputDir):
         os.makedirs(outputDir)
