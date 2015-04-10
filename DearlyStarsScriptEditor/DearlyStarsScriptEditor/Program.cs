8111e5820d42c5975ea4b5492f183efab3f12240
Superfranci99 <superfranci99@gmail.com>
2015-04-10 13:43:29 +0200

    Select file from an OpenFileDialog

--- DearlyStarsScriptEditor/DearlyStarsScriptEditor/Program.cs
+++ DearlyStarsScriptEditor/DearlyStarsScriptEditor/Program.cs
@@ -1,16 +1,30 @@
-﻿using System;
+﻿#define DEBUG
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DearlyStarsScriptEditor
 {
     class Program
-    {
+    {   
+        [STAThread]
         static void Main(string[] args)
         {
-            Script sc = new Script(@"D:\Francesco\Documenti\GitHub\Dearly-Stars\Packs\FileData\system\154_AIH_GOP_MAIN01_MES.BBQ");
+            // when debugging open a file from a certain path
+            Script sc = null;
+#if DEBUG
+            sc = new Script(@"D:\Francesco\Documenti\GitHub\Dearly-Stars\Packs\FileData\system\154_AIH_GOP_MAIN01_MES.BBQ");
+#else
+            OpenFileDialog op = new OpenFileDialog();
+            if (op.ShowDialog() == DialogResult.OK)
+                sc = new Script(op.FileName);
+            else
+                Application.Exit();
+#endif
             
             BBQEditor editor = new BBQEditor(sc);
             editor.ShowDialog();
